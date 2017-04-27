7115852d986783798a0fc339399cc95003e871a5
sourfeethsu <sourfeethsu@gmail.com>
2017-04-27 14:51:11 +0800

    Add photo resize

--- resources/views/product.blade.php
+++ resources/views/product.blade.php
@@ -14,7 +14,7 @@
   </div>
   <div class="photo-container">
     <div class="photo" id="show">
-      <span style="text-shadow: 0 0 2px white;" id="name"></span>
+      <span style="background-color: rgba(255, 255, 255, 0.5)" id="name"></span>
     </div>
     <div class="previews">
       @foreach ($photos as $photo)
@@ -24,17 +24,19 @@
   </div>
   <div class="photo-wall">
     @foreach ($photos as $photo)
-      <div class="photo-brick" src="{{$photo->path}}" style="background-image:url({{$photo->path}});"></div>
+      <div id="{{ $photo->name }}" class="photo-brick" src="{{$photo->path}}" style="background-image:url({{$photo->path}});"></div>
     @endforeach
   </div>
   <div id="modal" class="modal">
     <span class="close">&times;</span>
     <div id="modal-img" class="modal-image"></div>
+    <div id="modal-content" class="modal-content"></div>
   </div>
   <script type="text/javascript">
     var bricks = window.document.getElementsByClassName('photo-brick')
     var modal = window.document.getElementById('modal')
     var modalImg = window.document.getElementById('modal-img')
+    var modalContent = window.document.getElementById('modal-content')
     var closeIcon = window.document.getElementsByClassName('close')[0]
     for (var x=0; x<bricks.length; x++){
       bricks[x].onclick = showModal.bind(bricks[x])
@@ -42,6 +44,7 @@
     closeIcon.onclick = closeModal
     function showModal() {
       modal.style.display = "block"
+      modalContent.innerHTML = this.id
       modalImg.style.cssText = this.style.cssText;
     }
     function closeModal() {
